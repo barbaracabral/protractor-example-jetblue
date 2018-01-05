c366f9735fae904c75ccc92afac31cec48cbd841
t-bravi.barbara <t-bravi.barbara@nexxera.com>
2018-01-05 13:15:04 -0200

    Fixing some issues

--- pages/index.page.js
+++ pages/index.page.js
@@ -2,13 +2,16 @@
 
 class IndexPage{
   constructor(){
-    //this.filtro = element(by.dataTest('filtro'));
+    this.filtro = element(by.dataTest('filtro'));
   }
 
   visit(){
     return browser.get('/#/');
   }
 
+  insertDepartDate(nome){
+    return this.filtro.sendKeys(nome);
+  }
 
 }
 module.exports = IndexPage;
--- spec/finding_flights.js
+++ spec/finding_flights.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const IndexPage = require('../pages/index.page.js');
 //const POKEMON = require('../../data/db.js');
 
@@ -7,6 +8,23 @@ describe('Filtro de Voos', ()=> {
 
   it('Deve pesquisar vôos saindo de Florianópolis para São Paulo', ()=>{
     indexPage.visit();
+    element(by.id('jbBookerDepart')).clear();
+    element(by.id('jbBookerDepart')).sendKeys('Florianópolis');
+    element(by.model('departCal.date')).sendKeys('12-29-2017');
+    element(by.id('jbBookerArrive')).sendKeys('São Paulo');
+    element(by.model('returnCal.date')).sendKeys('12-29-2017');
+
+    element(by.buttonText('Find it')).click();
+
+    var someObj = element(by.binding('errorMessage'));
+
+    console.log(someObj);
+  //  console.dir(someObj);
+
+    //expect(element(by.binding('errorMessage'))).to.equal('Please enter valid departure city.');
+
+    browser.sleep('35000');
+
     //indexPage.pesquisarPokemon('mew');
     //expect(listagemPage.resultados.count()).toEqual(2);
   });
